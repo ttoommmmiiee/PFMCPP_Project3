e9b6018737f4f9ca1a86171903b859f9391ca3fa
matkatmusic <matkatmusic@gmail.com>
2021-04-29 13:07:10 -0700

    Updated instructions

--- main.cpp
+++ main.cpp
@@ -7,7 +7,100 @@ Create a branch named Part2
 
  tasks
  0) delete all of the plain english pseudo-code you added in Part1.
+   don't forget to remove the blank lines left behind after you remove your comments
    - you should be left with only your UDTs.
+*/
+// example:
+// if you had something like this at the end of Part1e:
+/*
+Thing: Car Wash   
+    5 properties:
+        - number of vacuum cleaners
+        - number of eco-friendly cleaning supplies
+        - stores the amount of water used per week.
+        - stores amount of profit made per week
+        - number of cars serviced per day
+    3 things it can do:
+        - wash and wax car
+        - charge customer
+        - detail the car interior
+ */
+
+#include <iostream>
+#include <string>
+namespace Part1eVersion 
+{
+struct CarWash        
+{
+    //number of vacuum cleaners                     
+    int numVacuumCleaners = 3; 
+    //number of eco-friendly cleaning supplies      
+    int numEcoFriendlyCleaningSupplies = 20;     
+    //stores the amount of water used per week.     
+    float waterUsedPerWeek = 200.f;            
+    //stores amount of profit made per week         
+    float profitPerWeek = 495.95f;               
+    //number of cars serviced per day               
+    int numberOfCarsServiced = 10;               
+    
+    struct Car  
+    {
+        bool isAPickupTruck = false;
+        float gasMileage = 26.2f;        
+        int year = 1985;
+        std::string manufacturer = "Toyota";
+        std::string model = "Corolla";
+
+        void fillTank(double fuelAmountInGallons = 2.0);  
+        void breakDown(std::string failureType, bool requiresTow = false);
+        int getMilesTraveledAnnually(bool includeUberLyftTrips);
+    };
+
+    //wash and wax car
+    void washAndWaxCar( Car car ); 
+    //charge customer
+    float chargeCustomer(float discountPercentage);
+    //detail the car interior
+    void detailInterior( Car car );
+    
+    Car carBeingServiced;  
+};
+}
+
+//this is what I want to see after the code is cleaned up: 
+namespace Part2Version
+{
+struct CarWash        
+{
+    int numVacuumCleaners = 3; 
+    int numEcoFriendlyCleaningSupplies = 20;     
+    float waterUsedPerWeek = 200.f;            
+    float profitPerWeek = 495.95f;               
+    int numberOfCarsServiced = 10;               
+    
+    struct Car  
+    {
+        bool isAPickupTruck = false;
+        float gasMileage = 26.2f;        
+        int year = 1985;
+        std::string manufacturer = "Toyota";
+        std::string model = "Corolla";
+
+        void fillTank(double fuelAmountInGallons = 2.0);  
+        void breakDown(std::string failureType, bool requiresTow = false);
+        int getMilesTraveledAnnually(bool includeUberLyftTrips);
+    };
+
+    void washAndWaxCar( Car car ); 
+    float chargeCustomer(float discountPercentage);
+    void detailInterior( Car car );
+    
+    Car carBeingServiced;  
+};
+}
+  /*
+    The above snippet is just an example showing you how to clean up your code.  
+    Do not put your cleaned up code into a namespace like I have done here.
 
  1) write the definition for the Type that leftFoot and rightFoot are instantiations of.
     don't forget to define and implement the member functions 'stepForward()' and 'stepSize()'
@@ -21,11 +114,16 @@ Create a branch named Part2
 
  /*
  2) provide implementations for the member functions you declared in your 10 user-defined types from the previous video outside of your UDT definitions.
+    If you have warnings about 'unused parameter', you aren't using one of your function parameters in your implementation.
+    use the parameter in your implementation.
+    If you have warnings about 'overshadow', a local variable in the function has the same name as a class member.
+    change the name of your local variable so it is different than the class member's name.
  
  3) be sure to write the correct full qualified name for the nested type's member functions.
  
  4) After you finish defining each type/function, click the [run] button.  Clear up any errors or warnings as best you can.
  */
+
 
 
 
